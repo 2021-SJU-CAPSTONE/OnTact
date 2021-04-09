c3fe99d203e2d6242b955e92061a42d5ccd6443e
jangjaeko <jangjaeko45@gmail.com>
2021-04-09 14:39:53 +0900

    ts update

--- Web/src/components/views/Navbar/NavBar.tsx
+++ Web/src/components/views/Navbar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import LeftMenu from './Sections/LeftMenu';
+
 import { Drawer, Button} from 'antd';
 import './Sections/Navbar.css';
 import Icon from '@ant-design/icons';
@@ -9,7 +9,7 @@ import {UserOutlined} from '@ant-design/icons';
 import {Link} from 'react-router-dom';
 function NavBar() {
   
-
+  const userName : string = '김국문';
   return (
     <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%',height:'60px' }}>
       <div  className="menu__logo">
@@ -17,11 +17,11 @@ function NavBar() {
           <img src={logo}  style= {{marginTop: '-10px',marginLeft :'50px',width: '100px', height : '30px'}}/>
         </Link>
       </div>
-      <div style={{float :' right '}}>
-        <h6 style={{marginTop:'20px',marginRight :'50px'}}>logout</h6>
+      <div style={{float :'right',marginTop :'20px'}}>
+        <h6 style={{marginRight :'50px'}}>logout</h6>
       </div>
-      <div style={{float :'right'}}>
-      <h6 style={{marginTop:'20px',marginRight :'50px',fontWeight : 'bold'}} ><UserOutlined style={{verticalAlign: 'top'}}/> 이주혁님 </h6>
+      <div style={{float :'right',marginTop :'20px'}}>
+      <h6 style={{marginRight :'50px',fontWeight : 'bold'}} ><UserOutlined style={{verticalAlign: 'top'}}/> {userName} </h6>
       </div>
       
     </nav>
