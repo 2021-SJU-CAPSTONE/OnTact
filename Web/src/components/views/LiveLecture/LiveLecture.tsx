4b66e3d11f20476fe42e0e4240bdb4e30dedbd4b
BeADia <koc0819@gmail.com>
2021-05-05 06:02:17 +0900

    add LiveVideo component

--- Web/src/components/views/LiveLecture/LiveLecture.tsx
+++ Web/src/components/views/LiveLecture/LiveLecture.tsx
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Chatting from "./Chatting/Chatting";
 import ChatWrite from "./Chatting/ChatWrite";
+import LiveVideo from '../LiveVideo/LiveVideo'
+import {socket} from './socket'
+
 function LiveLecture() {
+  const [clientId,setClientId] = useState("");
+
+  useEffect(()=>{
+    socket.on("init", (data:any) => setClientId(data.id)).emit("init"); 
+  },[]);
+  // function을 인자로 넘김으로 useState 변수를 넘길 수 있다.
+  const startCall = (isCaller:boolean, friendIf:string, confing:any) => {
+    socket.on("init",(data:any)=> {
+      console.log(data);
+      setClientId(data.id);
+    })
+  };
+
   return (
     <div style={{ paddingTop: "50px", minHeight: "calc(100vh - 80px" }}>
       <div className="row d-flex ">
@@ -9,11 +25,14 @@ function LiveLecture() {
           style={{
             width: "60%",
             height: "60vh",
-            backgroundColor: "black",
+            border : 'solid',
+            // backgroundColor: "black",
             float: "left",
             marginLeft: "50px",
           }}
-        ></div>
+        >
+          <LiveVideo clientId={clientId} startCall={startCall}/>
+        </div>
         <div>
           <Chatting />
         </div>
@@ -24,7 +43,8 @@ function LiveLecture() {
             marginLeft: "50px",
             width: "60%",
             height: "13vh",
-            backgroundColor: "gray",
+            border : 'solid',
+            // backgroundColor: "gray",
           }}
         ></div>
       </div>
