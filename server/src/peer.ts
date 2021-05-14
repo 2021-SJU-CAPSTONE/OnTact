6ebb01c3cc7beb8df692a720ecbf87308c995420
BeADia <koc0819@gmail.com>
2021-05-14 15:57:06 +0900

    temp

--- server/src/peer.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import peer from "peer";
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const serv = app.listen(5000);
-app.use("/peerjs", peer.ExpressPeerServer(serv));
